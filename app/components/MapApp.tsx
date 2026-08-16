05b3b29987f7c84d05e1b704107f0805ffc7193d
GitHubに登録したユーザ名/Users/tei/opt/anaconda3/envs/aidemy_python_3-9-18/bin/python <t.tsukashi@gmail.com>
2026-08-16 18:21:04 +0900

    モバイル版ポップアップのサブテキストをPC版と統一

--- app/components/MapApp.tsx
+++ app/components/MapApp.tsx
@@ -294,7 +294,7 @@ export default function MapApp() {
                 群馬県おでかけまっぷ
               </p>
               <p className="text-xs text-gray-500 leading-relaxed" style={{ whiteSpace: 'nowrap' }}>
-                県民のための週末お出かけプラットフォーム
+                群馬県の週末おでかけプラットフォーム
               </p>
               <a
                 href="https://docs.google.com/forms/d/e/1FAIpQLSfjd2ErqEMLI7gDMk4O5iutIRSUMI6AD0hkJSnN3tAT5UjIXA/viewform"
