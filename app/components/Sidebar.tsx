1ab8a39190e189e5c7ed510424826dd69d61db8f
GitHubに登録したユーザ名/Users/tei/opt/anaconda3/envs/aidemy_python_3-9-18/bin/python <t.tsukashi@gmail.com>
2026-08-23 22:23:50 +0900

    ロゴをlogo-pc_05に変更・トリミング・サブタイトル色調整

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -140,8 +140,8 @@ export default function Sidebar({
       {/* タイトル（サイドバーモードのみ） */}
       {!isSheet && (
         <div className="pt-1 pb-4 pr-4 pl-[22px] border-b border-gray-200">
-          <img src="/logo-pc_04.png" alt="GUNMAP" className="h-[52px] w-auto -ml-[23px]" />
-          <p className="text-xs text-black -mt-3 leading-relaxed ml-[39px]">
+          <img src="/logo-pc_05.png" alt="GUNMAP" className="h-[52px] w-auto -ml-[8px]" />
+          <p className="text-xs -mt-1 leading-relaxed ml-[39px]" style={{ color: '#1F1F1F' }}>
             群馬県の週末おでかけプラットフォーム
           </p>
         </div>
