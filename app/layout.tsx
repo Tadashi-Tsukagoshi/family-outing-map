0f0e3292f18b852e072a316a96dbc5eae4bf0fa4
GitHubに登録したユーザ名/Users/tei/opt/anaconda3/envs/aidemy_python_3-9-18/bin/python <t.tsukashi@gmail.com>
2026-08-23 22:34:42 +0900

    OGP画像をgunmap_OGP_02に変更、タイトル表記をGUNMAP｜グンマップに統一

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const shipporiMincho = Shippori_Mincho({
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://gunma-odekakemap.jp'),
-  title: 'GUNMAP / グンマップ',
+  title: 'GUNMAP ｜グンマップ',
   description: '今週末、家族でどこいく？県民がつくる、群馬のおでかけプラットフォーム',
   manifest: '/manifest.json',
   icons: {
@@ -29,13 +29,13 @@ export const metadata: Metadata = {
   },
   appleWebApp: {
     capable: true,
-    title: 'GUNMAP / グンマップ',
+    title: 'GUNMAP ｜グンマップ',
     statusBarStyle: 'default',
   },
   openGraph: {
     images: [
       {
-        url: '/ogp.png',
+        url: '/gunmap_OGP_02.png',
         width: 1200,
         height: 630,
       },
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: 'summary_large_image',
-    images: ['/ogp.png'],
+    images: ['/gunmap_OGP_02.png'],
   },
 }
 
